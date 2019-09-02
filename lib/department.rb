f03c108d746ac0dc1e092cd9cef95582cba8a0d6
John Travers <johnk.travers@gmail.com>
2019-09-02 16:51:40 -0600

    Add department class

---
+++ lib/department.rb
@@ -0,0 +1,18 @@
+class Department
+  attr_reader :name, :employees, :expenses
+
+  def initialize(name)
+    @name = name
+    @employees = []
+    @expenses = 0
+  end
+
+  def hire(employee)
+    @employees.push(employee)
+  end
+
+  def expense(cost)
+    @expenses += cost
+  end
+
+end
